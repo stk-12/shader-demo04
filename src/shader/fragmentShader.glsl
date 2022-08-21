bc973756c4eea556e1356d184d3312b9fefc789d
stk-12 <37107406+stk-12@users.noreply.github.com>
2022-08-21 17:51:55 +0900

    Initial commit

---
+++ src/shader/fragmentShader.glsl
@@ -0,0 +1,26 @@
+varying vec2 vUv;
+
+uniform float uTime;
+
+uniform sampler2D uTex;
+
+void main() {
+  vec2 uv = vUv;
+  // vec3 color = vec3(uv.x, uv.y, 0.5);
+  // vec3 color = vec3(uv.x + sin(uTime * 0.5) * 0.1, uv.y + cos(uTime * 2.0) * 0.1, 0.7);
+  // vec3 color = vec3(uv.x + sin(uTime * 1.5) / (uv.y - 0.5), uv.y + cos(uTime * 1.5) / (uv.x - 0.5), 0.6);
+
+  // float t = uTime * 2.0;
+  // vec2 uvOffset = vec2(cos(uv.y * 20.0 + t), sin(uv.x * 10.0 - t)) * 0.01;
+  // vec3 color = texture2D(uTex, vUv).rgb;
+  // vec3 color = texture2D(uTex, vUv + uvOffset).rgb;
+
+  float shift = sin(uTime * 4.0) * 0.01;
+  float r = texture2D(uTex, vUv + vec2(shift, 0.0)).r;
+  float g = texture2D(uTex, vUv).g;
+  float b = texture2D(uTex, vUv - vec2(shift, 0.0)).b;
+  vec3 color = vec3(r, g, b);
+
+  // gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+  gl_FragColor = vec4(color, 1.0);
+}
